0133221cc6590fcf59fa62b0be1a6e0e6f7535c0
seochan99 <gmlcks0513@naver.com>
2021-04-08 22:22:36 +0900

    hmm

--- sns-project/sns_project/main/views.py
+++ sns-project/sns_project/main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def showmain(request):
     return render(request,"main/show.html")
+
+def hello(request):
+    userName = request.GET("name")
+    return render(request,"main/hello.html",{"userName" : userName})
