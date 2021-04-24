95b553d3dd825f780ef24d676e623dbde14d234c
seochan99 <gmlcks0513@naver.com>
2021-04-24 13:09:22 +0900

    2 session

--- sns-project/sns_project/sns_project/settings.py
+++ sns-project/sns_project/sns_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os 
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,6 +118,7 @@ USE_TZ = False #표준시간대(UTC)로 사용 ?
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ( os.path.join('static'), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
