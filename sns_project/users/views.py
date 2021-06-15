290c3a19afa87f5e193199356ff49d2c6f77ffe8
seochan99 <gmlcks0513@naver.com>
2021-06-15 22:54:29 +0900

    users

--- sns_project/users/views.py
+++ sns_project/users/views.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import User
 def mypage(request):
     user = request.user
     posts = Post.objects.filter(writer = user) #로그인한 유저이름과 글 작성자 이름이 동일한 글 필터링한다.
-    return render(request,'user/mypage.html',{'posts':posts})
+    return render(request,'users/mypage.html',{'posts':posts})
