b8c8aa01539b76dae2a2fbc91bc39b7b0bb52c7d
seochan99 <gmlcks0513@naver.com>
2021-07-18 19:23:54 +0900

    follow/2

--- sns_project/users/urls.py
+++ sns_project/users/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name ="users"
 urlpatterns=[
     path('mypage/<int:id>',views.mypage,name="mypage"),
+    path('follow<int:id>',views.follow, name="follow"),
 ] 
--- sns_project/users/views.py
+++ sns_project/users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from main.models import Post
 from django.contrib.auth.models import User 
 
@@ -7,5 +7,17 @@ def mypage(request, id):
     context={
         'user':user,
         'posts':Post.objects.filter(writer=user),
+        'followings':user.profile.followings.all(),
+        'followers':user.profile.followers.all(),
     }
     return render(request,'users/mypage.html',context) 
+
+def follow(request,id):
+    user = request.user #로그인한 유저(A)
+    followed_user = get_object_or_404(User,pk=id) #팔로우/언팔로우 할 유저 (B)
+    is_follower = user.profile in followed_user.profile.followers.all() #B의 팔로워 목록에 A가 있는지 판단 
+    if is_follower:
+        user.profile.followings.remove(followed_user.profile) #remove -> A의 팔로잉 목록에서 B삭제 
+    else:
+        user.profile.followings.add(followed_user.profile) #A팔로잉 목록에서 B추가 
+    return redirect('users:mypage',followed_user.id)
